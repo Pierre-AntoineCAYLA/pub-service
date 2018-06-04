e8c5a182d6867f669c3e7b2a14d65db0a449a373
Pierre-Antoine <you@example.com>
2018-06-04 14:59:47 +0200

    affichage pubs ouverts

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var jours = new Array('Monday', 'Tuesday','Wednesday','Thursday','Friday','Satur
 var date=new Date();
 var day=jours[date.getDay]
 var listePubs=pubService.listerPubs();
-var listePubsOuverts=pubService.listerPubsOuverts(day);
+var listePubsOuverts=pubService.listerPubsOuverts();
 
 console.log(day+' liste pub '+JSON.stringify(listePubs)+ ', liste pubs ouverts '+JSON.stringify(listePubsOuverts));
+
+var pubService = require('./services/pubs.service');
+module.exports = {
+services: {
+pubService : pubService
+}
+}
