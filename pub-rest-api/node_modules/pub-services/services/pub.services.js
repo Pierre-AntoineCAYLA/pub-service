6a23968cc01b40bcf612d88bc9d8192375ece37e
Pierre-Antoine <you@example.com>
2018-06-04 16:51:16 +0200

    fin tp2

---
+++ pub-rest-api/node_modules/pub-services/services/pub.services.js
@@ -0,0 +1,23 @@
+var pubs=require('../mocks/pubs.json');
+var moment = require('moment');
+
+function listerPubs() {
+    return pubs;
+    }
+
+function listerPubsOuverts(){
+  var pubsOuverts=[];
+  pubs.forEach(pub=>{
+    if(pub['openDays'].includes(moment().format('dddd'))){
+      pubsOuverts.push(pub);
+    }
+  });
+    return pubsOuverts;
+}
+
+
+
+module.exports = {
+listerPubs: listerPubs,
+listerPubsOuverts: listerPubsOuverts
+}
